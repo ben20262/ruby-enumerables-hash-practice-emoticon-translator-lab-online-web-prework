1c1fc78e9ae453acfda666eb4370c0881b9db581
Benjamin Small <ben20262@aim.com>
2019-11-17 20:12:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file)
   emotes = YAML.load_file(file)
   meaning = emotes.keys
   cons = emotes.values
-  [[:get_meaning, meaning], [:get_emoticon, cons]].to_h
+  [[:get_meaning, emotes], [:get_emoticon, emotes]].to_h
 end
 
 def get_japanese_emoticon
