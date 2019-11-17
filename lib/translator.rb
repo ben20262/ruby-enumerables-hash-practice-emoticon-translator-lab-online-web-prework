5afebdfd17333c2fef4a486fd3876336506ba478
Benjamin Small <ben20262@aim.com>
2019-11-17 20:30:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,21 @@ require 'yaml'
 
 def load_library(file)
   emotes = YAML.load_file(file)
+  meaning = emotes.keys
+  cons = emotes.values
+  america = []
+  japan = []
+  mean_hash = {}
+  emo_hash = {}
+  x = 0
+  cons.each do |group|
+    america = group[0]
+    japan = group[1]
+    mean_hash[japan] = meaning[x]
+    emo_hash[america] = japan
+    x += 1
+  end
+  
   [[:get_meaning, emotes], [:get_emoticon, emotes]].to_h
 end
 
